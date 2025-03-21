1b6b3d2548305eb770a805c3486676646d3a2889
ericrpy <eric_rodrigues00@hotmail.com>
2025-03-21 09:13:34 -0300

    novo exercicio lista

---
+++ Lista.py
@@ -0,0 +1,24 @@
+# criando a lista
+lista = [10, 20, 30, 40, 50, 60]
+
+# alterando o índice 2
+lista[2] = 300
+
+# deletando o índice 3
+del lista[3]
+
+print (lista)
+
+# printando apenas o índice 2
+print(lista[2])
+
+# adicionando um índice no final da lista
+lista.append(50)
+
+# deletando o último índice da lista
+lista.pop()
+
+lista.append('BBB')
+
+print(lista)
+

a10407b5a06f79ab12eff15804769539cbba7690
ericrpy <eric_rodrigues00@hotmail.com>
2025-08-26 10:22:17 -0300

    try except

---
+++ List Comprehension.py
@@ -0,0 +1,14 @@
+# List Comprehension é uma forma rpida para criar listas a partir de iteráveis
+
+lista = [numero for numero in range(10)]
+print(lista)
+
+
+
+lista = [
+    numero * 2
+    for numero in range(10)]
+print(lista)
+
+
+
---
+++ try except.py
@@ -0,0 +1,12 @@
+a = 18
+b = 0
+
+try:
+    c = a/b
+    print(f'Resultado', c)
+
+except ZeroDivisionError: # trata apenas o erro ZeroDivisionError
+    print('Dividiu por zero')
+
+except Exception: # trata qualquer tipo de erro
+    print('Erro desconhecido')

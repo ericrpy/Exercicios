4ed1a62c8fc5a2a527769a836df6c80f6dc877af
ericrpy <eric_rodrigues00@hotmail.com>
2025-09-01 16:50:14 -0300

    novos exercicios

---
+++ Soma listas.py
@@ -0,0 +1,6 @@
+l1 = [1, 2, 3, 4, 5, 6, 7]
+l2 = [1, 2, 3, 4]
+
+lista_soma = [x + y for x, y in zip(l1, l2)]
+print(lista_soma)
+
---
+++ Unir listas.py
@@ -0,0 +1,3 @@
+l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+l2 = ['BA', 'SP', 'MG', 'RJ']
+print(list(zip(l1, l2)))
